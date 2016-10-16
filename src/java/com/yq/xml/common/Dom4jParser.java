1ad19d9be6891d3cd645db756a40d165c1ca9452
yqbjtu <yqbjtu@163.com>
2016-10-16 21:29:21 +0800

    read chinese character properly
    Change-Id: Id0d124c0ffc08fbd4cc18f7f5efedd4bb133c519

--- src/java/com/yq/xml/common/Dom4jParser.java
+++ src/java/com/yq/xml/common/Dom4jParser.java
@@ -2,12 +2,24 @@ package com.yq.xml.common;
 
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
@@ -18,23 +30,36 @@ public class Dom4jParser {
 		SAXReader reader = new SAXReader();
 		File xmlFile = new File(file);
 		Document doc = null;
-		try {
+		try {		    
 		   doc = reader.read(xmlFile);
-		} catch (DocumentException e) {
+		   doc.setXMLEncoding("UTF-8");
+		}catch (DocumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        
 		
-		Element root = doc.getRootElement();
-		StringBuffer rootValue = new StringBuffer()
-				.append("attr exportDate:" + root.attribute("exportDate").getValue());
-		List attrList = root.attributes();
-		int index = 0;
-		for(Object obj : attrList) {
-			Attribute attr = (Attribute) attrList.get(index);
-			index++;
-			rootValue.append(",Attr Name=" + attr.getName() + ", Value=" + attr.getValue());
-		}
+
+       /* String str = readFile(file);
+        try {
+            doc = DocumentHelper.parseText(str);
+            doc.setXMLEncoding("UTF-8");
+        }
+        catch (DocumentException e) {
+            e.printStackTrace();
+        }*/
+
+        Element root = doc.getRootElement();
+
+        StringBuffer rootValue = new StringBuffer()
+            .append("attr exportDate:" + root.attribute("exportDate").getValue());
+        List attrList = root.attributes();
+        int index = 0;
+        for (Object obj : attrList) {
+            Attribute attr = (Attribute)attrList.get(index);
+            index++;
+            rootValue.append(",Attr Name=" + attr.getName() + ", Value=" + attr.getValue());
+        }
 	    
 		System.out.println("root " + rootValue );
 		
@@ -46,7 +71,11 @@ public class Dom4jParser {
 		String operValue = "text:["+ operator.getText() + "], attr role=[" + operator.attributeValue("role") +"]";
 		System.out.println("operator " + operValue );
 		
-		
+		//需要在Eclipse的perference中的General--Workspace-Text file encoding中将编码设置为utf-8,否则中文显示和读取有问题
+	    Element cnElement = root.element("项目");
+	    String cnValue = "text:["+ cnElement.getText() + "], attr 语言=[" + cnElement.attributeValue("语言") +"]";
+	    System.out.println("项目 " + cnValue );
+	        
 		Element data = root.element("data");
 		List dataList = data.elements("book");
 		System.out.println("book size in data element:[" + dataList.size() + "]");
@@ -86,5 +115,37 @@ public class Dom4jParser {
 			}		
 		}		
 		
+	}
+	
+	private String readFile(String file) {
+	    FileInputStream inStream;
+        try {
+            inStream = new FileInputStream(file);
+            String myString = IOUtils.toString(inStream, "UTF-8");
+            System.out.println("myString:" + myString);
+
+            InputStreamReader isRead = new InputStreamReader(new FileInputStream(file), "utf-8");
+            String line; // 用来保存每行读取的内容
+            BufferedReader reader = new BufferedReader(isRead);
+            line = reader.readLine(); // 读取第一行
+            StringBuffer buf = new StringBuffer();
+            while (line != null) {
+                buf.append(line);
+                buf.append("\n");
+                line = reader.readLine();
+            }
+            reader.close();
+            isRead.close();
+
+            return myString;
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return "";  
 	}
 }
