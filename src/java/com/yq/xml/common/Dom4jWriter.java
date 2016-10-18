ff68ca716ce733a7dfff90d8aabce0a3dbf288e8
yqbjtu <yqbjtu@163.com>
2016-10-18 13:36:18 +0800

    finish writing the books.xml, which is used in our parsing demo
    Change-Id: I91a1e24249c228790f33f261d53cf1be1ca2eba5

--- src/java/com/yq/xml/common/Dom4jWriter.java
+++ src/java/com/yq/xml/common/Dom4jWriter.java
@@ -53,19 +53,55 @@ public class Dom4jWriter {
         comment.setText("This is for java xml parse 例子");
         comment.addCDATA("你好 & hello");
         
+        Element operator = root.addElement("operator");
+        operator.setText("root");
+        operator.addAttribute("role", "admin");
+
+        Element data = root.addElement("data");
         
+        for(int i = 1; i < 5; i++) {
+            Element book = data.addElement("book");
+            book.addAttribute("bookid", i + "");
+            book.addAttribute("mark",  "00" + i + "m");
+            
+            Element name = book.addElement("name");
+            Element price = book.addElement("price");
+            if (i == 1) {
+                name.setText("Netty in action");
+                price.setText("35");
+            }
+            else {
+                name.setText("book-name" + i);
+                price.setText(15 + "");
+            }    
+        }
+
+        Element book4 = (Element)data.elements("book").get(3);
+        Attribute attrMark = book4.attribute("mark");
+        if (attrMark != null) {
+            book4.remove(attrMark);
+        }
+
+        Element position = book4.addElement("position");
+        position.addAttribute("url", "http://www.jd.com");
+        for(int i=1; i < 3 ;i++ ) {
+            Element property = position.addElement("property");
+            property.addAttribute("name", "prop" + i);
+            property.setText("value0" + i);
+        }
+
         //为了将来的xml文件格式清晰，保留缩进，使用prettyprint
         OutputFormat format = OutputFormat.createPrettyPrint();
         format.setEncoding("UTF-8");
         try {
             //if format is not needed, you can omit it. 为了支持中文，我们必须使用utf8和FileOutputStream
-            
+
             //XMLWriter xmlWriter = new XMLWriter(new FileWriter(fileName), format);
             //---使用fileWriter无法正确保存中文字符因为java中由Writer类继承下来的子类没有提供编码格式处理
             XMLWriter xmlWriter = new XMLWriter(new FileOutputStream(xmlOutputFile), format);
             xmlWriter.write(doc);
             xmlWriter.close();
-            
+
             //XMLWriter consoleWriter = new XMLWriter(new OutputStreamWriter(System.out, "UTF-8"), format);
             XMLWriter consoleWriter = new XMLWriter(System.out, format);
             consoleWriter.write(doc);
